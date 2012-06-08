07d563cbd074f52e16c9a8d76f40d95be288c940
Adrian Delgado <adrian.sen@gmail.com>
2012-06-08 19:28:12 +0200

    Spring 1 - busqueda

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   # GET /places
   # GET /places.json
   def index
-    @places = Place.all
+    @places = Place.search(params[:search])
 
     respond_to do |format|
       format.html # index.html.erb
@@ -80,4 +80,11 @@ class PlacesController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  #SEARCH  
+  #def findplaces   
+  #  condicion = "name like '%#{params[:var]}%'"
+  #  @placesfound = Place.find(:all,:conditions => condicion)
+  #end
+
 end
